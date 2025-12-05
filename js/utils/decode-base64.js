0195f359ac4ffdf0b2bf3b6a16e8b862d6088ec0
Anders Wik <about@anderswik.se>
2025-12-05 18:11:15 +0100

    Moving decode-base64 page

---
+++ js/utils/decode-base64.js
@@ -0,0 +1,14 @@
+const ui = {
+    decodeBtn: document.getElementById("decode-btn"),
+    encodeBtn: document.getElementById("encode-btn")
+}
+
+ui.decodeBtn.addEventListener("click", function(){
+    var text = document.getElementById("encoded-text").value;
+    document.getElementById("decoded-text").value = atob(text);
+});
+
+ui.encodeBtn.addEventListener("click", function(){
+    var text = document.getElementById("decoded-text").value;
+    document.getElementById("encoded-text").value = btoa(text);
+});
